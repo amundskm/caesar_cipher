9c640cbc87b21d9ebcc5536dab37aee90454eafa
Keith Amundson <amundskm@gmail.com>
2018-08-15 09:51:22 -0500

    move get above function

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 #require 'sinatra/reloader'
 
+get '/' do
+    original = params['message']
+    shift = params['shift'].to_i
+
+    translated = cipher(original, shift) if original != nil
+    erb :index, :locals => {:translated => translated, :original => original}
+end
+
 def cipher(str, shift)
     str_array = str.split(//)
     new_str = []
@@ -9,20 +17,12 @@ def cipher(str, shift)
         if char =~ /[a-z]/
             new_char = ((char.ord + shift) -122) % 26 + 96
         elsif char =~ /[A-Z]/
-            new_char = ((char.ord + shift) - 90) % 26 + 64 
+            new_char = ((char.ord + shift) - 90) % 26 + 64
         else
-            new_char = char.ord 
+            new_char = char.ord
         end
         new_str << new_char.chr
     end
 
     new_str.join
-end
-
-get '/' do
-    original = params['message']
-    shift = params['shift'].to_i
-    
-    translated = cipher(original, shift) if original != nil
-    erb :index, :locals => {:translated => translated, :original => original}
 end
